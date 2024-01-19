0f4a08ed858ca82c4d251baf5bc599aee3eae3d2
PraeThanaphorn <156441715+PraeThanaphorn@users.noreply.github.com>
2024-01-19 15:05:35 -0500

    Update Lab0.R
    Update codes

--- Lab0.R
+++ Lab0.R
@@ -98,7 +98,7 @@ subset(RPI_Weather_Week,subset = snowed == TRUE)
 dec.snow <- order(-RPI_Weather_Week$temp)
 dec.snow
 
-#create an empty dataframe
+#Create an empty dataframe
 empty.DataFrame <- data.frame()
 v1 <- 1:10
 v1
@@ -108,49 +108,7 @@ v2 <- letters[1:10]
 df <- data.frame(col.name.1=v1, col.name.2=v2)
 df
 
-#import data and export data
-#write to a csv file
-write.csv(df, file='saved_df1.csv')
-df2<-read.csv('saved_df1.csv')
-df2
-
-####2010_EPI_data
-install.packages("csv")
-library(csv)
-data_2010EPI <- read.csv(file.choose(), header = T)
-data_2010EPI
-str(data_2010EPI)
-View(data_2010EPI)
-names(data_2010EPI) <- data_2010EPI[1,]
-data_2010EPI <- data_2010EPI[-1,]
-str(data_2010EPI)
-View(data_2010EPI)
-head(data_2010EPI)
-
-EPI_data_2010 = as.numeric(data_2010EPI$EPI)
-print(EPI_data_2010)
-summary(EPI_data_2010)
-fivenum(EPI_data_2010)
-boxplot(EPI_data_2010)
-hist(EPI_data_2010)
-
-
-rm(list = ls())
-######EPI_data
-#data_EPI <- read.csv(file.choose(), header = T) #header is a column's name.
-EPI_data <- read.csv("EPI_Data.csv")
-EPI_data
-View(EPI_data)
-str(EPI_data)
-summary(EPI_data)
-EPI <- EPI_data$EPI
-EPI
-boxplot(EPI)
-hist(EPI)
-fivenum(EPI)
-
-
-
+##Practice 3##
 temperature <- c(20,24.3,23,24.2,22.5)
 names(temperature) <- c("Mon","Tue","Web","Thur","Fri") #attach days to the temperature
 temperature
@@ -258,50 +216,43 @@ u
 runif(matrix(20))
 matrix(runif(20),nrow = 4)
 
-####
-days = c('Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun')
-temp = c(28, 30.5, 32, 31.2, 29.3, 27.9, 26.4)
-#snowed = c('T', 'T', 'F', 'F','T','T','F') #This is a mistake as we cannot convert the boolean to be a number (0 and 1)
-snowed = c(TRUE, TRUE, FALSE, FALSE, TRUE, TRUE, FALSE)
-class(snowed)
-help("data.frame")
-
-#Create data frame
-RPI_Weather_Week = data.frame(days, temp, snowed) #data frame function
-RPI_Weather_Week
-head(RPI_Weather_Week)
-
-str(RPI_Weather_Week)
-summary(RPI_Weather_Week)
-
-RPI_Weather_Week[1,]
-RPI_Weather_Week[,1]
-
-RPI_Weather_Week[,'snowed']
-RPI_Weather_Week[,'days']
-RPI_Weather_Week[,'temp']
-RPI_Weather_Week[1:5, c("days","temp")]
-
-RPI_Weather_Week$temp
-RPI_Weather_Week$days
-subset(RPI_Weather_Week,subset = snowed == TRUE)
-RPI_Weather_Week[sorted.snowed,]
-
-dec.snow <- order(-RPI_Weather_Week$temp)
-dec.snow
-
-#create an empty dataframe
-empty.DataFrame <- data.frame()
-v1 <- 1:10
-v1
-
-letters
-v2 <- letters[1:10]
-df <- data.frame(col.name.1=v1, col.name.2=v2)
-df
-
-#import data nad export data
+#import data and export data
 #write to a csv file
 write.csv(df, file='saved_df1.csv')
 df2<-read.csv('saved_df1.csv')
 df2
+
+##Practice 4##
+# Import 2010_EPI_data
+install.packages("csv")
+library(csv)
+data_2010EPI <- read.csv(file.choose(), header = T)
+data_2010EPI
+str(data_2010EPI)
+View(data_2010EPI)
+names(data_2010EPI) <- data_2010EPI[1,]
+data_2010EPI <- data_2010EPI[-1,]
+str(data_2010EPI)
+View(data_2010EPI)
+head(data_2010EPI)
+
+EPI_data_2010 = as.numeric(data_2010EPI$EPI)
+print(EPI_data_2010)
+summary(EPI_data_2010)
+fivenum(EPI_data_2010)
+boxplot(EPI_data_2010)
+hist(EPI_data_2010)
+
+rm(list = ls())
+# Import EPI_data
+#data_EPI <- read.csv(file.choose(), header = T) #header is a column's name.
+EPI_data <- read.csv("EPI_Data.csv")
+EPI_data
+View(EPI_data)
+str(EPI_data)
+summary(EPI_data)
+EPI <- EPI_data$EPI
+EPI
+boxplot(EPI)
+hist(EPI)
+fivenum(EPI)
