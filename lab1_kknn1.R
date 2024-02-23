ba707c90741d9bdef79ee7d90b801728d19728b6
PraeThanaphorn <156441715+PraeThanaphorn@users.noreply.github.com>
2024-02-23 11:09:16 -0500

    Add files via upload

---
+++ lab1_kknn1.R
@@ -0,0 +1,28 @@
+install.packages("rpart")
+library(rpart)
+
+install.packages("rpart.plot")
+library(rpart.plot)
+
+#iris dataset
+iris #show all observation points
+dim(iris)
+str(iris)
+head(iris)
+
+install.packages("kknn")
+library(kknn)
+require(kknn)
+
+m <- dim(iris)[1]
+val <- sample(1:m, size = round(m/3), replace = FALSE, 
+	prob = rep(1/m, m)) 
+iris.learn <- iris[-val,]
+iris.valid <- iris[val,]
+iris.kknn <- kknn(Species~., iris.learn, iris.valid, distance = 1,
+	kernel = "triangular")
+summary(iris.kknn)
+fit <- fitted(iris.kknn)
+table(iris.valid$Species, fit)
+pcol <- as.character(as.numeric(iris.valid$Species))
+pairs(iris.valid[1:4], pch=pcol, col=c("green3","red")[(iris.valid$Species !=fit)+1])
