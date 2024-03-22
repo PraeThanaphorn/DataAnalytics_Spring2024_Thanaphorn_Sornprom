10bd213816de374c07bdc85db066f4b0b3870d0f
PraeThanaphorn <156441715+PraeThanaphorn@users.noreply.github.com>
2024-03-22 11:37:10 -0400

    Add files via upload

---
+++ K-flod example with Auto dataset.R
@@ -0,0 +1,15 @@
+#K-flod cross validation with Auto dataset
+library(ISLR)
+library(MASS)
+install.packages("DAAG")
+library(DAAG)
+set.seed(17)
+help("rep")
+
+str(Auto)
+cv.error.10 = rep(0,10)
+for (i in 1:10){
+  glm.fit=glm(mpg~poly(horsepower, i), data=Auto)
+  cv.error.10[i]=cv.glm(Auto, glm.fit, K=10)$delta[1]
+}
+cv.error.10
